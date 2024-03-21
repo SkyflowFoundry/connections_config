827ac8296ddd172e5cda432fc2dace7cb9835228
joicejohnskyflow <135670291+joicejohnskyflow@users.noreply.github.com>
2024-03-21 12:59:45 -0700

    Update skyflow_connections_setup.py

--- skyflow_connections_setup.py
+++ skyflow_connections_setup.py
@@ -9,7 +9,8 @@ import sys
 
 # Change the below 6 constants according to your vault and RequestBin entries
 SKYFLOW_OUTBOUND_CONNECTION_ENDPOINT = str(os.environ.get('SKYFLOW_OUTBOUND_CONNECTION_ENDPOINT', ''))
-VAULT_OWNER_SA_CREDENTIALS = "Bearer " + str(os.environ.get('VAULT_OWNER_SA_CREDENTIALS', ''))
+# VAULT_OWNER_SA_CREDENTIALS = "Bearer " + str(os.environ.get('VAULT_OWNER_SA_CREDENTIALS', ''))
+VAULT_OWNER_SA_CREDENTIALS = "Bearer sky-l1451-y89c8350d6cc4537b87b45e769aee365"
 print("VAULT_OWNER_SA_CREDENTIALS :", VAULT_OWNER_SA_CREDENTIALS)
 VAULT_ID = str(os.environ.get('VAULT_ID', ''))  # Replace with your Vault ID
 SKYFLOW_ACCOUNT_ID = str(os.environ.get('SKYFLOW_ACCOUNT_ID', ''))  # Replace Account ID with your Account ID
