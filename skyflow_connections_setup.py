9c7b07f620724845bfdbd5fa5e511b779f03d542
joicejohnskyflow <135670291+joicejohnskyflow@users.noreply.github.com>
2024-03-21 17:28:52 -0700

    Update skyflow_connections_setup.py

--- skyflow_connections_setup.py
+++ skyflow_connections_setup.py
@@ -2,16 +2,7 @@ import os
 import json
 import requests
 import sys
-'''
-# Define constants
-# Change the below 6 constants according to your vault and RequestBin entries
-SKYFLOW_OUTBOUND_CONNECTION_ENDPOINT = str(os.environ.get('SKYFLOW_OUTBOUND_CONNECTION_ENDPOINT', 'https://manage.skyflowapis.com/v1/gateway/outboundRoutes'))
-SKYFLOW_ACCOUNT_ID = str(os.environ.get('SKYFLOW_ACCOUNT_ID', ''))  # Your Account ID
-VAULT_ID = str(os.environ.get('VAULT_ID', ''))  # Your Vault ID
-VAULT_OWNER_SA_CREDENTIALS = str(os.environ.get('VAULT_OWNER_SA_CREDENTIALS', '')) # Your API key
-REQUEST_BIN_BASE_URL = str(os.environ.get('REQUEST_BIN_BASE_URL', 'https://ens3s06g2e69r.x.pipedream.net'))
-REQUEST_BIN_RELATIVE_PATH = str(os.environ.get('REQUEST_BIN_RELATIVE_PATH', '/sample/post/request/'))
-'''
+
 SKYFLOW_OUTBOUND_CONNECTION_ENDPOINT = str(os.environ.get('SKYFLOW_OUTBOUND_CONNECTION_ENDPOINT', 'https://manage.skyflowapis.com/v1/gateway/outboundRoutes'))
 VAULT_OWNER_SA_CREDENTIALS = 'Bearer ' + str(os.environ.get('VAULT_OWNER_SA_CREDENTIALS', '')) # Your API key
 VAULT_ID = str(os.environ.get('VAULT_ID', ''))  # Your Vault ID
